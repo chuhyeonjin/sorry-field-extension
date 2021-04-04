00fd49bc4bb75cddbc964999bd2f92a55941a126
Chu Hyeonjin <choo51455145@gmail.com>
2021-04-04 13:14:27 +0900

    콘피그 매니저 구조 변경

--- src/configManager.ts
+++ src/configManager.ts
@@ -1,6 +1,18 @@
+function getValue(key: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(key, (value) => {
+      resolve(value);
+    });
+  });
+}
+
+function setValue(key: string, value: any) {
+  chrome.storage.local.set({ [key]: value }, () => {});
+}
+
 export function getAutoLoginEnable(): Promise<boolean> {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get('autoLoginEnable', ({ autoLoginEnable }) => {
+    getValue('autoLoginEnable').then(({ autoLoginEnable }) => {
       const isUndefined = autoLoginEnable === undefined;
       autoLoginEnable = isUndefined ? true : autoLoginEnable;
       if (isUndefined) {
@@ -12,5 +24,5 @@ export function getAutoLoginEnable(): Promise<boolean> {
 }
 
 export function setAutoLoginEnable(autoLoginEnable: boolean) {
-  chrome.storage.local.set({ autoLoginEnable: autoLoginEnable }, () => {});
+  setValue('autoLoginEnable', autoLoginEnable);
 }
